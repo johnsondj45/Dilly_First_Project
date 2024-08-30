ef1c776d003f6f3fed5a1d9f2fdbf963afd6f70f
Dylan Johnson <johnsondj45@gmail.com>
2024-08-30 12:24:36 -0400

    Reset Project to baseline state

--- app/src/main/java/com/example/dillysfirstproject/activities/MainActivity.kt
+++ app/src/main/java/com/example/dillysfirstproject/activities/MainActivity.kt
@@ -1,14 +1,11 @@
-package com.example.dillysfirstproject
+package com.example.dillysfirstproject.activities
 
 import android.app.Activity
 import android.os.Bundle
 import com.example.dillysfirstproject.databinding.MainActivityBinding
 
 class MainActivity : Activity() {
-
     private lateinit var binding: MainActivityBinding
-    var newText = "This String Has Been Updated."
-    var secondNewText = "New String,"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,22 +13,6 @@ class MainActivity : Activity() {
         val view = binding.root
         setContentView(view)
     }
-
-    override fun onResume() {
-        super.onResume()
-    }
-
-    fun setUpUserInterface() {
-        binding.firstbutton.setOnClickListener{
-            updateText()
-        }
-    }
-
-    private fun updateText() {
-        binding.newText.text = (newText)
-    }
-
-
 }
 
 
